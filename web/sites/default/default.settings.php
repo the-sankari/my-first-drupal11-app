e8fb72d239726f3854410d2b5d38ce6a977cf4b7
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-09-04 23:17:52 +0300

    Views

--- web/sites/default/default.settings.php
+++ web/sites/default/default.settings.php
@@ -871,5 +871,3 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-
-$config['system.logging']['error_level'] = 'verbose';
