79907a10b2a1c29da279e27ddde6c252dc902e2d
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-10-06 19:56:39 +0300

    Update on module files

--- web/modules/custom/palindrome_checker/src/controller/PalindromeCheckerController.php
+++ web/modules/custom/palindrome_checker/src/controller/PalindromeCheckerController.php
@@ -3,44 +3,33 @@
 namespace Drupal\palindrome_checker\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Palindrome Checker Controller.
  */
-class PalindromeCheckerController extends ControllerBase
-{
+class PalindromeCheckerController extends ControllerBase {
 
-    /**
-     * Check if the given input is a palindrome.
-     */
-    public function check(Request $request)
-    {
-        $output = '';
+  /**
+   * API to check if the given input is a palindrome.
+   */
+  public function apiCheck(Request $request) {
+    // Get the input from the JSON body.
+    $data = json_decode($request->getContent(), TRUE);
+    $input = $data['input'] ?? '';
 
-        // Get the input from the query parameter.
-        $input = $request->query->get('input', '');
-
-        if (!empty($input)) {
-            // Call the palindrome checker function.
-            $is_palindrome = palindrome_checker_is_palindrome($input);
-
-            if ($is_palindrome) {
-                $output = $this->t('The string "%input" is a palindrome.', ['%input' => $input]);
-            } else {
-                $output = $this->t('The string "%input" is NOT a palindrome.', ['%input' => $input]);
-            }
-        }
-
-        // Simple form for input.
-        $form = '<form method="GET" action="">
-               <label for="input">Enter a string:</label>
-               <input type="text" id="input" name="input" value="' . htmlspecialchars($input) . '">
-               <input type="submit" value="Check">
-             </form>';
-
-        return new Response($form . '<p>' . $output . '</p>');
+    if (empty($input)) {
+      return new JsonResponse(['error' => 'Input string is required'], 400);
     }
+
+    // Call the palindrome checker function (ensure this function exists in your module).
+    $is_palindrome = palindrome_checker_is_palindrome($input);
+
+    return new JsonResponse([
+      'input' => $input,
+      'is_palindrome' => $is_palindrome
+    ]);
+  }
 
 }
